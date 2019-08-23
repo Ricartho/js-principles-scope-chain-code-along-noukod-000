d8225881454d55f54cf416a66fc3d42ac97ad28b
Ricartho Franck <ricartho.franck@esih.edu>
2019-08-23 14:54:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function upperCaseCustomerName() {
   return customerName = customerName.toUpperCase()
 }
 
-// function changeLeastFavoriteCustomer(){
-//   leastFavoriteCustomer = 'sour fred';
-// }
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'sour fred';
+}
 
 
 function setBestCustomer(){
