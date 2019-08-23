d9e1c7d3b0ba3573ae58ce6c5c2d1d7413df2804
Ricartho Franck <ricartho.franck@esih.edu>
2019-08-23 15:06:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function overWriteBestCustomer(newFavorite){
 // }
 
 function attemptTwoFavoriteCustomers(){
-  let favoriteCustomer;
+    let favoriteCustomer = 'bar';
     let favoriteCustomer = 'not bar';
     
 }
